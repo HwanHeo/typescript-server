f984a802c63bc5eae7ee980300ce3e6deb866407
HeoHwan <mnnmnnm7@gmail.com>
2018-07-14 22:13:11 +0900

    Update tsconfig.json and server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import * as express from 'express';
+import http from 'http';
+import express from 'express';
 
 export class Server {
     app: express.Application;
     
     constructor() {
-        this.app = express();
+        this.app = express();        
     }
 }
+
+const server: Server = new Server();
+
+http.createServer(server.app).listen(3000, () => {    
+    console.log('App is running in the Background');
+});
